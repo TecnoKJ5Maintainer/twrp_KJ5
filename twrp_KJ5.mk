7603b35ab57754578d5fb589e4b31af3d2a35363
Re6ee <re6eeken@gmail.com>
2025-08-28 19:05:41 +0800

    fixed twrp_KJ5.mk

--- twrp_KJ5.mk
+++ twrp_KJ5.mk
@@ -15,8 +15,8 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from TECNO-KJ5 device
 $(call inherit-product, device/tecno/KJ5/device.mk)
 
-PRODUCT_DEVICE := twrp_KJ5
-PRODUCT_NAME := KJ5
+PRODUCT_DEVICE := KJ5
+PRODUCT_NAME := twrp_KJ5
 PRODUCT_BRAND := Tecno
 PRODUCT_MODEL := Tecno KJ5
 PRODUCT_MANUFACTURER := TECNO
